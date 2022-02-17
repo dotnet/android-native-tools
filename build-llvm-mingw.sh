5012546f402f1ca4d2176af921edc3cc09001429
Marek Habersack <grendel@twistedcode.net>
2022-02-17 19:04:57 +0100

    Let's see if it works

--- build-llvm-mingw.sh
+++ build-llvm-mingw.sh
@@ -4,7 +4,7 @@ MY_DIR="$(cd $(dirname $0);pwd)"
 
 source ${MY_DIR}/common.sh
 
-MY_OUTPUT_DIR="${ARTIFACTS_DIR}/llvm-mingw/${HOST}"
+MY_OUTPUT_DIR="${MINGW_BASE_OUTPUT_DIR}/${HOST}"
 LLVM_MINGW_DIR="${MY_DIR}/external/llvm-mingw"
 
 function prepare_sources()
@@ -38,7 +38,19 @@ function build()
 	./build-openmp.sh $PREFIX
 }
 
+function gather_versions()
+{
+	local mingw_ver=$(sed -n -E -e 's/^AC_INIT\(\[.*\],\[(.*)\],.*\)/\1/p' < mingw-w64/configure.ac)
+	local llvm_ver=$(detect_llvm_version llvm-project)
+
+	cat <<EOF > "${MINGW_VERSION_INFO_PATH}"
+L:${llvm_ver}
+M:${mingw_ver}
+EOF
+}
+
 create_dir "${MY_OUTPUT_DIR}"
 
 (cd "${LLVM_MINGW_DIR}"; prepare_sources)
 (cd "${LLVM_MINGW_DIR}"; build "${MY_OUTPUT_DIR}")
+(cd "${LLVM_MINGW_DIR}"; gather_versions)
