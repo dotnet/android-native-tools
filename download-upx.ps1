f19d8143afd0a65b5a51568ae44d3518c59acbc7
Marek Habersack <grendel@twistedcode.net>
2023-07-26 11:20:49 +0200

    Bump LLVM version to 16.0.6 and UPX to 4.0.2
    Context: https://github.com/dotnet/maui/issues/14531
    Context: https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-erref/596a1078-e883-4972-9bbc-49e60bebca55
    Context: https://github.com/upx/upx/issues/594
    
    `llc.exe` (and `lld.exe` as well, potentially) appear to sometimes die
    because of access violation (NTSTATUS code 0xC0000005) on Windows 10 and
    Windows 11.  The current suspicion is that it's either the UPX stub
    produced by UPX 3.96 we've been using so far that crashes, or that
    there's something wrong with LLVM 15.x.
    
    Since we want to update LLVM to v16 anyway, this commit bumps it to
    16.0.6 at the same time updating UPX to version 4.0.2 which appears to
    have fixes for a similar crash.

--- download-upx.ps1
+++ download-upx.ps1
@@ -1,7 +1,7 @@
-$UpxVer="3.96"
+$UpxVer="4.0.2"
 
 $progresspreference = 'silentlyContinue'
-Invoke-WebRequest https://github.com/upx/upx/releases/download/v$UpxVer/upx-3.96-win64.zip -OutFile upx.zip
+Invoke-WebRequest https://github.com/upx/upx/releases/download/v$UpxVer/upx-$UpxVer-win64.zip -OutFile upx.zip
 $progressPreference = 'Continue'
 
 $7zipPath="C:\Program Files\7-Zip\7z.exe"
