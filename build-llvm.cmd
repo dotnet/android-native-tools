ea36b415b83ef6f26c6662a6436ed82dec3b11b6
Marek Habersack <grendel@twistedcode.net>
2024-03-06 12:33:27 +0100

    Don't rename llvm-objcopy on Windows

--- build-llvm.cmd
+++ build-llvm.cmd
@@ -4,8 +4,8 @@ set SOURCE_DIR=%MY_DIR%external\llvm\llvm
 
 set PROJECTS=lld
 set TARGETS=X86;ARM;AArch64
-set BINARIES=llvm-mc.exe llvm-strip.exe lld.exe llc.exe
-set PDBS=llvm-mc.pdb llvm-strip.pdb lld.pdb llc.pdb
+set BINARIES=llvm-mc.exe llvm-objcopy.exe lld.exe llc.exe
+set PDBS=llvm-mc.pdb llvm-objcopy.pdb lld.pdb llc.pdb
 
 set HOST_BUILD_DIR=%BUILD_DIR%\%HOST%\llvm
 set HOST_BIN_DIR=%HOST_BUILD_DIR%\Release\bin
@@ -67,12 +67,6 @@ IF %ERRORLEVEL% GEQ 1 EXIT /B 4
 
 msbuild /p:Configuration=Release /m tools\llc\llc.vcxproj
 IF %ERRORLEVEL% GEQ 1 EXIT /B 5
-
-move %HOST_BIN_DIR%\llvm-objcopy.exe %HOST_BIN_DIR%\llvm-strip.exe
-IF %ERRORLEVEL% GEQ 1 EXIT /B 6
-
-move %HOST_BIN_DIR%\llvm-objcopy.pdb %HOST_BIN_DIR%\llvm-strip.pdb
-IF %ERRORLEVEL% GEQ 1 EXIT /B 7
 
 for %%b in (%BINARIES%) DO (
   copy %HOST_BIN_DIR%\%%b %HOST_ARTIFACTS_BIN_DIR%\%%b
