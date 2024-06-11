8de48aa35a062c575ced0c783b5f131289a15793
Marek Habersack <grendel@twistedcode.net>
2024-06-11 19:04:08 +0200

    Set C flags in addition to the C++ ones

--- build-common.cmd
+++ build-common.cmd
@@ -6,5 +6,7 @@ set HOST_ARTIFACTS_DIR=%ARTIFACTS_DIR%\%HOST%
 set HOST_ARTIFACTS_BIN_DIR=%HOST_ARTIFACTS_DIR%\bin
 set CMAKE_VS_GENERATOR=Visual Studio 17 2022
 set MSVC_RUNTIME_LIBRARY="MultiThreaded"
+set CXXFLAGS=/Qspectre /sdl /guard:cf
+set CFLAGS=/Qspectre /sdl /guard:cf
 
 mkdir %HOST_ARTIFACTS_BIN_DIR%
